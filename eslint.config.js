39c205eaaabf5a346c9ebedbb94b026368bd3845
Fernando Lamas <lamas.fernando1419@gmail.com>
2025-03-26 19:41:00 -0300

    Improve linter config and fix files

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
+import angularEslint from "@angular-eslint/eslint-plugin";
+import angularEslintTemplate from "@angular-eslint/eslint-plugin-template";
 import js from "@eslint/js";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import jestPlugin from "eslint-plugin-jest";
 
@@ -23,8 +25,11 @@ export default [
          },
       },
       plugins: {
-         "@typescript-eslint": tsPlugin,
          "jest": jestPlugin, // enable jest rules
+         "@angular-eslint": angularEslint,
+         // "@typescript-eslint": tsPlugin,
+         "@typescript-eslint": tsEslint,
+
       },
       rules: {
          "no-unused-vars": "off",
@@ -33,6 +38,56 @@ export default [
          "@typescript-eslint/explicit-function-return-type": "off",
          "@typescript-eslint/no-explicit-any": "warn",
          "@typescript-eslint/no-inferrable-types": "off",
+         "@typescript-eslint/no-non-null-assertion": "error",
+         "eqeqeq": ["error", "always"],
+         "curly": ["error", "all"],
+         "no-const-assign": "error",
+         "no-var": "error",
+         "prefer-const": "error",
+         // "no-console": ["warn", { allow: ["warn", "error"] }],
+         "no-debugger": "error",
+         "no-trailing-spaces": "error",
+         "comma-dangle": ["error", "always-multiline"],
+         "quotes": ["error", "single", { "allowTemplateLiterals": true, "avoidEscape": true }],
+         "semi": ["error", "always"],
+         "indent": ["error", 3, { "SwitchCase": 1 }],
+         "object-curly-spacing": ["error", "always"],
+         "no-shadow": "error",
+         "no-duplicate-imports": "error",
+         "no-multiple-empty-lines": ["error", { max: 1 }],
+         "arrow-spacing": ["error", { before: true, after: true }],
+         "no-param-reassign": "error",
+         "no-return-await": "error",
+         "no-unsafe-finally": "error",
+         "prefer-template": "error",
+         "spaced-comment": ["error", "always"],
+         "yoda": "error",
+         "array-bracket-spacing": ["error", "never"],
+         "block-spacing": ["error", "always"],
+         "eol-last": ["error", "always"],
+         "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
+         "lines-between-class-members": ["error", "always"],
+         "no-mixed-spaces-and-tabs": "error",
+         "padded-blocks": ["error", "never"],
+         "space-in-parens": ["error", "never"],
+         "space-infix-ops": "error",
+         "space-unary-ops": ["error", { "words": true, "nonwords": false }],
+         "switch-colon-spacing": ["error", { "after": true, "before": false }],
+         // Angular
+         "@angular-eslint/no-input-rename": "error",
+         "@angular-eslint/no-output-rename": "error",
+         "@angular-eslint/no-lifecycle-call": "error",
+         "@angular-eslint/use-lifecycle-interface": "error",
+         // "@angular-eslint/prefer-on-push-component-change-detection": "warn"
+      },
+   },
+   {
+      files: ["*.html"],
+      plugins: {
+         "@angular-eslint/template": angularEslintTemplate,
+      },
+      rules: {
+         "@angular-eslint/template/no-negated-async": "error",
       },
    },
    {
